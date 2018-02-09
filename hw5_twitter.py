1b59acf4305f5033f5c6444f6aae4c8a86838de9
Anndo Ko <handcuffanndo@gmail.com>
2018-02-09 03:52:21 -0500

    implement part 3: caching

--- hw5_twitter.py
+++ hw5_twitter.py
@@ -29,19 +29,62 @@ requests.get(url, auth = auth)
 #Write your code below:
 #Code for Part 3: Caching
 #Finish parts 1 and 2 and then come back to this
+## data request & caching
+twitter_cache_file = "twitter_cache.json" # set up a file for caching
+try:
+    cache_file = open(twitter_cache_file, 'r')
+    cache_content = cache_file.read()
+    CACHE_DICTION = json.loads(cache_content)
+    cache_file.close()
+except:
+    CACHE_DICTION = {}
+
+## to generate a unique id of a request
+def unique_id_generator(base_url, params_diction):
+    alphabetized_keys = sorted(params_diction.keys())
+
+    lst = []
+    for key in alphabetized_keys:
+        lst.append("{}-{}".format(key, params_diction[key]))
+
+    unique_id = base_url + "_".join(lst) # combine the baseurl and the formatted pairs of keys and values
+    return unique_id # return a unique id of the request
+
+## get data from Twitter
+def request_twitter_data(username, num_tweets):
+    # set up the base URL
+    base_url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+
+    # set up a dictionary of parameters
+    params_diction = {}
+    params_diction["screen_name"] = username
+    params_diction["count"] = num_tweets
+
+    # generate a unique id of this search
+    unique_id = unique_id_generator(base_url, params_diction)
+
+    # request/cache data
+    if unique_id in CACHE_DICTION:
+        print("Getting data from the cache file...")
+        return(CACHE_DICTION[unique_id])
+    else:
+        print("Making new data request...")
+        response = requests.get(url = base_url, params = params_diction, auth = auth)
+        results_lst = json.loads(response.text) # results_lst is a list of dictions (tweets)
+        results_dic = {} # create a diction to store the results_lst
+        results_dic["statuses"] = results_lst
+
+        CACHE_DICTION[unique_id] = results_dic
+
+        cache_file = open(twitter_cache_file,"w")
+        cache_string = json.dumps(CACHE_DICTION)
+        cache_file.write(cache_string)
+        cache_file.close()
+        return CACHE_DICTION[unique_id]
 
 #Code for Part 1: Get Tweets
-## set up the url and params for requesting data
-baseurl = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-params = {
-    "screen_name": username,
-    "count": num_tweets
-    }
 ## request data
-response = requests.get(baseurl, params = params, auth = auth) # request the data
-results_lst = json.loads(response.text) # results_lst is a list of dictions (tweets)
-results_dic = {} # create a diction to store the results_lst
-results_dic["statuses"] = results_lst
+results_dic = request_twitter_data(username, num_tweets) # request the data
 
 ## write the data in a file
 results_file = "tweets.json" # file name: tweets.json
@@ -56,7 +99,6 @@ f.close() # close the file
 for tweet in results_dic["statuses"]:
     tokenized_text = nltk.word_tokenize(tweet["text"]) # tokenize the words in the tweet
     words_dic = nltk.FreqDist(tokenized_text) # get a frequency distribution of the tokenized list
-
 
 real_words_lst = [] # create a list to store real words:
 ignore_lst = ["http", "https", "RT"] # create a list of words that should be ignored
